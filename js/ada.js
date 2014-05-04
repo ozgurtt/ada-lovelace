4f0f98da9a8273f481585a1a50ea96468de4177a
luccb <lbrochu@gmail.com>
2014-05-03 19:58:02 -0600

    Background image for Ivo's programming menu added. Second level added for testing.

--- js/ada.js
+++ js/ada.js
@@ -20,65 +20,40 @@ var mainState = {
     game.load.image('library', 'assets/library.jpg');
     game.load.image('ground', 'assets/platform.png');
     game.load.image('introScreen', 'assets/intro-exterior.jpg');
-    //game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
 	game.load.image('ada', 'assets/ada.png');
 	game.load.image('punchcard', 'assets/punchcard.png');
 	game.load.image('ivo', 'assets/ivo.png');
 	game.load.image('exitDoor','assets/blue-door.png');
-
+	game.load.image('punchcardMenu','assets/punchcard-menu.jpg');
 	},
 
 
 	create: function() {
-    	//  We're going to be using physics, so enable the Arcade Physics system
     	game.physics.startSystem(Phaser.Physics.ARCADE);
-
-    	//  A simple background for our game
+		//level backrground
     	game.add.sprite(0, 0, 'library');
-
-    	//  The platforms group contains the ground and the 2 ledges we can jump on
+		//platforms and ground
     	platforms = game.add.group();
-
-		//  We will enable physics for any object that is created in this group
 		platforms.enableBody = true;
-	
-		// Here we create the ground.
 		var ground = platforms.create(0, game.world.height - 64, 'ground');
-	
-		//  Scale it to fit the width of the game (the original sprite is 400x32 in size)
+		//double width to fit screen resolution
 		ground.scale.setTo(2, 2);
-	
-		//  This stops it from falling away when you jump on it
 		ground.body.immovable = true;
+		
 		doors=game.add.group();
 		doors.enableBody=true;
 		var exitDoor = doors.create(780, game.world.height - 130, 'exitDoor');
-		//exitDoor.scale.setTo(2,2);
 		
 		exitDoor.body.immovable=true;
-		
-			
-		//  Now let's create two ledges
-		//var ledge = platforms.create(400, 400, 'ground');
-		//ledge.body.immovable = true;
 	
-		//ledge = platforms.create(-150, 250, 'ground');
-		//ledge.body.immovable = true;
-	
-		// The player and its settings
 		player = game.add.sprite(32, game.world.height - 150, 'ada');
-		machine = game.add.sprite(400, game.world.height - 113, 'ivo');
-		
-		game.physics.arcade.enable(machine);
-		//  We need to enable physics on the player
 		game.physics.arcade.enable(player);
-	
-		//  Player physics properties. Give the little guy a slight bounce.
 		player.body.bounce.y = 0.2;
 		player.body.gravity.y = gravity;
 		player.body.collideWorldBounds = true;
-		//machine.body.bounce.y=0.2;
-		//machine.body.gravity.y=gravity;
+		
+		machine = game.add.sprite(400, game.world.height - 113, 'ivo');
+		game.physics.arcade.enable(machine);
 		machine.body.collideWorldBounds = true;
 		machine.body.immovable = true;
 		//TODO: When Ivo moves, set immovable false, and give it gravity.
@@ -89,65 +64,109 @@ var mainState = {
 		punchcard.body.gravity.y = 300;
 		punchcard.body.bounce.y=0.3;
 		
-		//  Here we'll create 12 of them evenly spaced apart
-		/*for (var i = 0; i < 12; i++)
-		{
-			//  Create a star inside of the 'stars' group
-			var punchcard = punchcards.create(i * 70, 0, 'punchcard');
-	
-			//  Let gravity do its thing
-			punchcard.body.gravity.y = 300;
-	
-			//  This just gives each star a slightly random bounce value
-			punchcard.body.bounce.y = 0.7 + Math.random() * 0.2;
-		}*/
-		
-		
-		
-		//  The score
 		scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
-	
-		//  Our controls.
 		cursors = game.input.keyboard.createCursorKeys();		
 	},
 	
 	update: function() {
-	
-		//  Collide the player and the stars with the platforms
 		game.physics.arcade.collide(player, platforms);
 		game.physics.arcade.collide(player, doors);
 		game.physics.arcade.collide(punchcards, platforms);
 		game.physics.arcade.collide(machine, platforms);
 		game.physics.arcade.collide(player, machine,inputCode, null, this);
-		//  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
 		game.physics.arcade.overlap(player, punchcards, collectPunchcard, null, this);
 		
-		//  Reset the players velocity (movement)
+		//  Stop Ada
 		player.body.velocity.x = 0;
 	
 		if (cursors.left.isDown)
 		{
-			//  Move to the left
 			player.body.velocity.x = -250;
-	
-			//player.animations.play('left');
 		}
 		else if (cursors.right.isDown)
 		{
-			//  Move to the right
 			player.body.velocity.x = 250;
-	
-			//player.animations.play('right');
 		}
-		else
+		if (cursors.up.isDown && player.body.touching.down)
 		{
-			//  Stand still
-			player.animations.stop();
-	
-			//player.frame = 4;
+			player.body.velocity.y = -450;
 		}
+
+	}
+}
+var level2 = {
+
+    preload: function() {
+    game.load.image('library', 'assets/library.jpg');
+    game.load.image('ground', 'assets/platform.png');
+    game.load.image('introScreen', 'assets/intro-exterior.jpg');
+	game.load.image('ada', 'assets/ada.png');
+	game.load.image('punchcard', 'assets/punchcard.png');
+	game.load.image('ivo', 'assets/ivo.png');
+	game.load.image('exitDoor','assets/blue-door.png');
+	game.load.image('punchcardMenu','assets/punchcard-menu.jpg');
+	},
+
+
+	create: function() {
+    	game.physics.startSystem(Phaser.Physics.ARCADE);
+		//level backrground
+    	game.add.sprite(0, 0, 'library');
+		//platforms and ground
+    	platforms = game.add.group();
+		platforms.enableBody = true;
+		var ground = platforms.create(0, game.world.height - 64, 'ground');
+		//double width to fit screen resolution
+		ground.scale.setTo(2, 2);
+		ground.body.immovable = true;
 		
-		//  Allow the player to jump if they are touching the ground.
+		doors=game.add.group();
+		doors.enableBody=true;
+		var exitDoor = doors.create(780, game.world.height - 130, 'exitDoor');
+		
+		exitDoor.body.immovable=true;
+	
+		player = game.add.sprite(32, game.world.height - 500, 'ada');
+		game.physics.arcade.enable(player);
+		player.body.bounce.y = 0.2;
+		player.body.gravity.y = gravity;
+		player.body.collideWorldBounds = true;
+		
+		machine = game.add.sprite(400, game.world.height - 113, 'ivo');
+		game.physics.arcade.enable(machine);
+		machine.body.collideWorldBounds = true;
+		machine.body.immovable = true;
+		//TODO: When Ivo moves, set immovable false, and give it gravity.
+
+		punchcards = game.add.group();
+		punchcards.enableBody = true;
+		var punchcard=punchcards.create(600, 400, 'punchcard');
+		punchcard.body.gravity.y = 300;
+		punchcard.body.bounce.y=0.3;
+		
+		scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+		cursors = game.input.keyboard.createCursorKeys();		
+	},
+	
+	update: function() {
+		game.physics.arcade.collide(player, platforms);
+		game.physics.arcade.collide(player, doors);
+		game.physics.arcade.collide(punchcards, platforms);
+		game.physics.arcade.collide(machine, platforms);
+		game.physics.arcade.collide(player, machine,inputCode, null, this);
+		game.physics.arcade.overlap(player, punchcards, collectPunchcard, null, this);
+		
+		//  Stop Ada
+		player.body.velocity.x = 0;
+	
+		if (cursors.left.isDown)
+		{
+			player.body.velocity.x = -250;
+		}
+		else if (cursors.right.isDown)
+		{
+			player.body.velocity.x = 250;
+		}
 		if (cursors.up.isDown && player.body.touching.down)
 		{
 			player.body.velocity.y = -450;
@@ -169,7 +188,10 @@ var menuState = {
 				this.game.state.start('main');
 		}
 	}
-game.state.add('main', mainState); 
+	
+	
+game.state.add('main', mainState);
+game.state.add('level2', level2); 
 game.state.add('menu', menuState); 
 game.state.start('menu'); 
 
@@ -180,5 +202,6 @@ function collectPunchcard (player, punchcard) {
 
 }
 function inputCode (player, machine){
-	scoreText.text = 'Ada touches ivo';
+	game.add.sprite(0,0,'punchcardMenu');
+	//this.game.state.start('level2'); //TEST code
 }
