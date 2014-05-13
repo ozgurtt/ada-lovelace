5543c2ecf3a0bdf23da6c3d01680a15b9324695e
luccb <lbrochu@gmail.com>
2014-05-12 20:07:07 -0600

    Ivo Menu more complete. exit allows first level to be completed

--- js/ada.js
+++ js/ada.js
@@ -8,11 +8,22 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '');
 var player;
 var platforms;
 var doors;
+var exitDoor;
+var exitLight;
 var cursors;
 var machine;
 var ivoMenu;
 var ivoButtonAdd;
 var ivoCommands;
+var ivoMenuButtons=new Object();
+
+/*var ivoMenuEject;
+var ivoMenuExit;
+var ivoMenuExecute;
+var ivoMenuMoveLeft;
+var ivoMenuMoveRight;
+var ivoMenuBlueDoor;
+var ivoMenuYellowDoor;*/
 var gravity=900;
 
 var punchcards;
@@ -28,11 +39,19 @@ var mainState = {
 	game.load.image('ada', 'assets/ada.png');
 	game.load.image('punchcard', 'assets/punchcard.png');
 	game.load.image('ivo', 'assets/ivo.png');
-	game.load.image('exitDoor','assets/blue-door.png');
+	game.load.image('blueDoor','assets/blue-door.png');
+	game.load.image('exitLight','assets/exit-light.png');
 	game.load.image('punchcardMenu','assets/punchcard-menu.jpg');
 	game.load.image('menuButtonAdd','assets/menu-button-add.jpg');
 	game.load.image('punchcardMenu','assets/punchcard-menu.jpg');
 	game.load.image('menuCommands','assets/menu-commands.png');
+	game.load.image('menuEject','assets/menu-eject-cards.png');
+	game.load.image('menuExit','assets/menu-exit.png');
+	game.load.image('menuExecute','assets/menu-execute.png');
+	game.load.image('menuMoveLeft','assets/menu-move-left.png');
+	game.load.image('menuMoveRight','assets/menu-move-right.png');
+	game.load.image('menuBlueDoor','assets/menu-open-blue-door.png');
+	game.load.image('menuYellowDoor','assets/menu-open-yellow-door.png');
 	},
 
 
@@ -50,8 +69,13 @@ var mainState = {
 		
 		doors=game.add.group();
 		doors.enableBody=true;
-		var exitDoor = doors.create(780, game.world.height - 130, 'exitDoor');
+		exitLight = game.add.sprite(780, game.world.height-130, 'exitLight');
+		game.physics.arcade.enable(exitLight);
+		exitLight.body.collideWorldBounds = true;
+		exitLight.body.immovable = true;
+		exitLight.exists=false;
 		
+		exitDoor = doors.create(780, game.world.height - 130, 'blueDoor');
 		exitDoor.body.immovable=true;
 	
 		player = game.add.sprite(32, game.world.height - 150, 'ada');
@@ -65,13 +89,29 @@ var mainState = {
 		machine.body.collideWorldBounds = true;
 		machine.body.immovable = true;
 		//TODO: When Ivo moves, set immovable false, and give it gravity.
-
+		
+		//setting up the menu and submenu for Ivo but not displaying until we need to.
+		//ordered so that the later images are on top
 		ivoMenu = game.add.sprite(0,0,'punchcardMenu');
-		ivoMenu.exists=false;
+		ivoMenu.visible=false;
 		ivoButtonAdd = game.add.button(15, 20, 'menuButtonAdd', clickButtonAdd);
 		ivoButtonAdd.visible = false;
+
+		ivoMenuButtons.exit=game.add.button(15,300,'menuExit');
+		ivoMenuButtons.exit.visible=false;
+		ivoMenuButtons.execute=game.add.button(15,500,'menuExecute');
+		ivoMenuButtons.execute.visible=false;
 		ivoCommands = game.add.sprite(40,40,'menuCommands');
-		ivoCommands.exists=false;
+		ivoCommands.visible=false;
+		ivoMenuButtons.moveLeft=game.add.button(45,100,'menuMoveLeft');
+		ivoMenuButtons.moveLeft.visible=false;
+		ivoMenuButtons.moveRight=game.add.button(45,200,'menuMoveRight');
+		ivoMenuButtons.moveRight.visible=false;
+		ivoMenuButtons.blueDoor=game.add.button(45,300,'menuBlueDoor',clickBlueDoor);
+		ivoMenuButtons.blueDoor.visible=false;
+		ivoMenuButtons.yellowDoor=game.add.button(45,400,'menuYellowDoor');
+		ivoMenuButtons.yellowDoor.visible=false;
+		
 		punchcards = game.add.group();
 		punchcards.enableBody = true;
 		var punchcard=punchcards.create(600, 400, 'punchcard');
@@ -89,7 +129,7 @@ var mainState = {
 		game.physics.arcade.collide(machine, platforms);
 		game.physics.arcade.collide(player, machine,inputCode, null, this);
 		game.physics.arcade.overlap(player, punchcards, collectPunchcard, null, this);
-		
+		game.physics.arcade.collide(player, exitLight,touchExitLight,null,this);
 		//  Stop Ada
 		player.body.velocity.x = 0;
 	
@@ -136,12 +176,26 @@ function collectPunchcard (player, punchcard) {
     punchcardText.text = 'Punchcards: ' + collectedPunchcards;
 
 }
-function inputCode (player, machine){
+function touchExitLight(player,exitLight){
+	alert("Level 1 complete");
+}
+function inputCode (player, machine){ //display menu and buttons when Ada is close to Ivo and presses enter
 	if(this.game.input.keyboard.isDown(Phaser.Keyboard.ENTER)){
-		ivoMenu.exists=true;
+		ivoMenu.visible=true;
 		ivoButtonAdd.visible=true;
+		ivoMenuButtons.exit.visible=true;
+		ivoMenuButtons.execute.visible=true;
 	}
 }
-function clickButtonAdd () {
-	ivoCommands.exists=true;
+function clickButtonAdd () { //display submenu and buttons
+	ivoCommands.visible=true;
+	ivoMenuButtons.moveLeft.visible=true;
+	ivoMenuButtons.moveRight.visible=true;
+	ivoMenuButtons.blueDoor.visible=true;
+	ivoMenuButtons.yellowDoor.visible=true;
+}
+function clickBlueDoor (){
+	alert('blue door button pressed');
+	exitDoor.exists=false;
+	exitLight.exists=true;
 }
