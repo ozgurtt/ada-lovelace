31d2195d4353150826358433ed6e2931be064729
luccb <lbrochu@gmail.com>
2014-07-13 13:55:41 -0600

    level 4 and 5 tweaks, reverse grav added

--- js/ada.js
+++ js/ada.js
@@ -17,6 +17,7 @@ var exitArrow;
 var cursors;
 var machine;
 var timer;
+var jumpVelocity=-480;
 //var timerIsRunning=false;
 var ivoMenu;
 var ivoCommandsBackground;
@@ -35,9 +36,10 @@ var commandQueue=[];
 var writePosition=0;//for writing new commands and reading through them
 var readPosition=0;
 var executeNextOnStop=false;
+var executeNextOnAdaStop=false;
 //var beginQueue=true;//only true when player clicks execute button, otherwise we're moving though automatically.
 
-var level=1;
+var level=5;//default 1, change here to move starting levels
 var mainState = {
     preload: function() {
 	game.stage.setBackgroundColor('#333');
@@ -45,10 +47,9 @@ var mainState = {
 	    font: '40px Helvetica',
 	    fill: '#999'
 	});
-    //game.load.image('plainBackground', 'assets/plain-background.png');
     game.load.image('ground', 'assets/platform.png');
 	game.load.image('spike', 'assets/spike.png');
-    game.load.image('introScreen', 'assets/intro-exterior.jpg');
+    //game.load.image('background', 'assets/background.jpg');
 	game.load.image('ada', 'assets/ada.png');
 	game.load.image('punchcard', 'assets/punchcard.png');
 	game.load.image('ivo', 'assets/ivo-drawn.png');
@@ -71,13 +72,15 @@ var mainState = {
 	game.load.tilemap('level-1', 'assets/level-1.json', null, Phaser.Tilemap.TILED_JSON);
 	game.load.tilemap('level-2', 'assets/level-2.json', null, Phaser.Tilemap.TILED_JSON);
 	game.load.tilemap('level-3', 'assets/level-3.json', null, Phaser.Tilemap.TILED_JSON);
+	game.load.tilemap('level-4', 'assets/level-4.json', null, Phaser.Tilemap.TILED_JSON);
+	game.load.tilemap('level-5', 'assets/level-5.json', null, Phaser.Tilemap.TILED_JSON);
     game.load.image('ada-tileset', 'assets/ada-tileset.png');
 	},
 
 	create: function() {		
     	game.physics.startSystem(Phaser.Physics.ARCADE);
 		game.stage.backgroundColor = '#bfbeb2';
-		//game.stage.backgroundColor = '#000';
+		//game.add.sprite(0, 0, 'background');
     	timer = game.time.create(false);
 		//  Set it to wait 5 seconds to trigger an event
 		//timer.add(5000, updateCounter, this);
@@ -88,7 +91,7 @@ var mainState = {
 		codeText = game.add.text(100,300,'code', { fontSize:'12px', fill: '#000' });
 		codeText.visible=false;
 		cursors = game.input.keyboard.createCursorKeys();
-		loadLevel(3);
+		loadLevel(level);
 						
 	},
 	
@@ -103,7 +106,7 @@ var mainState = {
 		game.physics.arcade.overlap(player, punchcards, collectPunchcard, null, this);
 		game.physics.arcade.collide(player, exitArrow, touchExitLight,null,this);
 		game.physics.arcade.collide(player, spikes, touchSpikes,null,this);	
-		if(game.physics.arcade.distanceBetween(player, machine)<100) {
+		if(game.physics.arcade.distanceBetween(player, machine)<130) {
 			if (!closeToIvo){
 				closeToIvo=true;
 				//tutorialText.text="press enter to program Ivo with punch cards";
@@ -123,32 +126,44 @@ var mainState = {
 		} else {
 			player.body.velocity.x = 0;
 		}
-		if ((machine.body.velocity.x==0) && (machine.body.velocity.y==0)) {
-			
+		if ((machine.body.velocity.x<5)&&(machine.body.velocity.x>-5) && (machine.body.onFloor())&&(machine.body.immovable==false)) {
+			//alert("close enough")
+			machine.body.velocity.x=0;
 			machine.body.immovable=true;//When stationary we don't want Ivo be pushed around.
 			if (executeNextOnStop) {
 				executeNextOnStop=false;
 				clickExecute();
 			}
 		}
-	
+		//if ((player.body.velocity.y<5)&&(player.body.velocity.y>-5)){
+			//executeNextonAdaStop=false;
+			//clickExecute();
+		//}
+			
 		if (cursors.left.isDown) {
 			player.body.velocity.x = -220;
-			//player.flipped=true;
+			player.scale.x = -1;
 		} else if (cursors.right.isDown) {
 			player.body.velocity.x = 220;
-			//player.flipped=false;
+			player.scale.x = 1;
 		}
 		if (cursors.up.isDown && (player.body.onFloor() || player.body.touching.down))//player may be standing on tiles or entities.  onFloor tests against tiles and body.touching other objects.
 		{
-			player.body.velocity.y = -480;//jump
+			player.body.velocity.y = jumpVelocity;
 		} else if (this.game.input.keyboard.isDown(Phaser.Keyboard.ESC)) {
 			hideCommandsMenu();
 			hideMenu();
+		} else if (((cursors.up.isDown)||(cursors.down.isDown))&&(player.body.blocked.up))//player may be standing on tiles or entities.  onFloor tests against tiles and body.touching other objects.
+		{
+			player.body.velocity.y = -jumpVelocity;
+		} else if (this.game.input.keyboard.isDown(Phaser.Keyboard.R)) {
+			alert("r or R");
 		}
+		
 		if (timer.running){
 			timerText.text='Waiting: ' + timer.duration.toFixed(0);
 		}
+
 		//if (!beginQueue){
 			//clickExecute();//check in update for next queue action, doing so within clickExecute causes problems.
 		//}
@@ -178,13 +193,13 @@ function collectPunchcard (player, punchcard) {
     punchcardText.text = 'Punchcards: ' + collectedPunchcards;
 }
 function touchExitLight(player,exitLight){
-	tutorialText.text="level complete";
+	//tutorialText.text="level complete";
 	level++;
 	loadLevel(level);
 }
 function touchSpikes(player,spikes){
-	tutorialText.text="ouch";
-	loadLevel(level);
+	//tutorialText.text="ouch";
+	loadLevel(level); //for testing. should be loadLevel(level);
 }
 function inputCode (player, machine){ //player can start programming when she's close to Ivo
 	if(this.game.input.keyboard.isDown(Phaser.Keyboard.ENTER)){
@@ -247,14 +262,21 @@ function clickExecute (){
 		}
 	} else if (commandQueue[readPosition]=="Move right"){
 		machine.body.immovable = false;
-		machine.body.velocity.x=650;
+		machine.body.velocity.x=490;
 		if (machine.body.touching.up) {
 			player.body.velocity.x=machine.body.velocity.x;
 		}
 		hideMenu();
+		if (commandQueue.length>readPosition+1){
+			readPosition++;
+			executeNextOnStop=true;
+		} else {
+			//beginQueue=true;
+			readPosition=0;
+		}
 	} else if (commandQueue[readPosition]=="Move left"){
 		machine.body.immovable = false;
-		machine.body.velocity.x=-650;
+		machine.body.velocity.x=-490;
 		if (machine.body.touching.up) {
 			player.body.velocity.x=machine.body.velocity.x;
 		}
@@ -273,6 +295,18 @@ function clickExecute (){
 		timerText.text='Waiting: ' + timer.duration.toFixed(0);
 		hideMenu();
 		
+	} else if (commandQueue[readPosition]=="Reverse Ada's gravity") {//testing: get this working first.
+		hideMenu();
+		//gravity=-1050;//for testing. should be gravity=(-gravity);
+		player.body.gravity.y=-gravity;
+		player.scale.y = -1;
+		if (commandQueue.length>readPosition+1){
+			readPosition++;
+			executeNextOnAdaStop=true;
+		} else {
+			//beginQueue=true;
+			readPosition=0;
+		}
 	}
 	//if (commandQueue.length>queuePosition+1){
 		//beginQueue=false;
@@ -280,15 +314,15 @@ function clickExecute (){
 	//}
 }
 function clickMoveRight(){
-	commandQueue[writePosition]="Move right";
 	hideCommandsMenu();
-	ivoMenuButtons.codeText.text+=commandQueue.length+": "+commandQueue[writePosition];
+	commandQueue[writePosition]="Move right";
+	ivoMenuButtons.codeText.text+=commandQueue.length+": "+commandQueue[writePosition]+"\n";
 	spendCard();
 }
 function clickMoveLeft(){
 	hideCommandsMenu();
 	commandQueue[writePosition]="Move left";
-	ivoMenuButtons.codeText.text+=commandQueue.length+": "+commandQueue[writePosition];
+	ivoMenuButtons.codeText.text+=commandQueue.length+": "+commandQueue[writePosition]+"\n";
 	spendCard();
 }
 function clickBlueDoor (){
@@ -316,7 +350,7 @@ function clickGravity (){
 	
 	commandQueue[0]="Reverse Ada's gravity";
 	hideCommandsMenu();
-	ivoMenuButtons.codeText.text+=commandQueue.length+": "+commandQueue[0];
+	ivoMenuButtons.codeText.text+=commandQueue.length+": "+commandQueue[writePosition]+"\n";
 	spendCard();
 }
 function showMenu() {
@@ -383,7 +417,7 @@ function loadLevel(level) {
 		});
 	}
 	
-	collectedPunchcards = 2;
+	collectedPunchcards = 5;
 	spentPunchcards=0;
 	map = game.add.tilemap('level-'+level);
 	map.addTilesetImage('ada-tileset');
@@ -425,6 +459,8 @@ function loadLevel(level) {
 	player.body.gravity.y = gravity;
 	player.body.collideWorldBounds = true;
 	player.body.maxVelocity.y = 500;//keep her from falling through tiles
+	player.anchor.setTo(.5, .5); //so it flips around its middle
+ 	
 	game.camera.follow(player);
 	//player.bringToTop();//sprite was hiding behind new tilemaps being created each level.
 	//player.position.setTo(32,200);
@@ -432,9 +468,9 @@ function loadLevel(level) {
 	game.physics.arcade.enable(machine);
 	machine.body.collideWorldBounds = true;
 	machine.body.immovable = false;
-	machine.body.gravity.y = gravity-500;//ensures Ada will stick to the top of Ivo while they fall, she has more gravity.
+	machine.body.gravity.y = gravity-200;//ensures Ada will stick to the top of Ivo while they fall, she has more gravity.
 	//machine.body.maxVelocity.x=50;<-slow Ivo is fun to play with. Special level?
-	machine.body.maxVelocity.y=120;//Hack because Ada's gravity pushed Ivo down too fast in mid air.
+	machine.body.maxVelocity.y=150;//Hack because Ada's gravity pushed Ivo down too fast in mid air.
 	machine.anchor.setTo(0.25,0);//for calculating distance between player and Ivo
 	//setting up the menu and submenu for Ivo but not displaying until we need to.
 	//ordered so that the later images are on top
